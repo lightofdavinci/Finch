18a1ab38adf0e33191d6ad04738bdbdb632aaf96
Dan Volosnikov <danvolosnikov@gmail.com>
2017-04-03 22:02:20 -0400

    first commit

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/css', express.static(process.cwd() + '/public/css'));
+app.use('/images', express.static(process.cwd() + '/public/images'));
+app.use('/scripts', express.static(process.cwd() + '/public/scripts'));
+app.use('/fonts', express.static(process.cwd() + '/public/fonts'));
 app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.get('/', function(req, res){
